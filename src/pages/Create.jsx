f076c24d821a2cabae4c3dfbaac460dcde2b5a79
calebscode <calebomalley@ufl.edu>
2024-01-02 08:38:24 -0500

    icons

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,59 +1,34 @@
-import { Container, Button, ButtonGroup, Stack, Typography } from "@mui/material";
-
-function handleClick() {
-  console.log('you clicked me!')
-}
+import SendIcon from '@mui/icons-material/Send';
+import AcUnitOutlinedIcon from '@mui/icons-material/AcUnitOutlined';
+import { Container } from '@mui/material';
+import { Button } from '@mui/material';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 export default function Create() {
   return (
     <Container>
-      <Typography
-        variant="h6"
-        component="h2"
-        color="textSecondary"
-        gutterBottom>
-        Create A New Note
-      </Typography>
-      <Stack spacing={2} direction="row">
-        <Button variant="text">text</Button>
-        <Button variant="contained">contained</Button>
-        <Button variant="outlined">outlined</Button>
-        <Button variant="contained" disabled>disabled</Button>
-      </Stack>
+      <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon color='primary'/>
+      <AcUnitOutlinedIcon color='secondary' fontSize='large'/>
+      <AcUnitOutlinedIcon color='action' fontSize='small'/>
+      <AcUnitOutlinedIcon color='error'/>
+      <AcUnitOutlinedIcon color='disabled'/>
+
       <br></br>
-      <Stack spacing={2} direction="row">
-        <Button color="secondary" variant="text">text</Button>
-        <Button color="secondary" variant="contained">contained</Button>
-        <Button color="secondary" variant="outlined">outlined</Button>
-        <Button color="secondary" variant="contained" disabled>disabled</Button>
-      </Stack>
-      <Typography gutterBottom>
-        Primary Color Button Group Default
-      </Typography>
-      <ButtonGroup color="primary">
-        <Button>button 1</Button>
-        <Button>button 2</Button>
-        <Button>button 3</Button>
-        <Button>button 4</Button>
-      </ButtonGroup>
-      <Typography>
-        Secondary Color Button Group Contained
-      </Typography>
-      <ButtonGroup color="secondary">
-        <Button variant="contained">button 1</Button>
-        <Button variant="contained">button 2</Button>
-        <Button variant="contained">button 3</Button>
-        <Button variant="contained">button 4</Button>
-      </ButtonGroup>
-      <Typography>
-        Submit Button with Event Listener
-      </Typography>
+
       <Button
-        type="submit"
-        color="secondary"
-        variant="contained"
-        onClick={handleClick}>
+        type='submit'
+        color='secondary'
+        variant='contained'
+        endIcon={<SendIcon />}>
         Submit
+      </Button>
+
+      <Button
+        variant='outlined'
+        color='primary'
+        startIcon={<DeleteOutlineIcon/>}>
+        Delete
       </Button>
     </Container>
   )
