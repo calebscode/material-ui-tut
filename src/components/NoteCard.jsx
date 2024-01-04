4b00c6f699a68201073a6d9c487cfcf942f55b2b
Caleb O'Malley <calebomalley@ufl.edu>
2024-01-04 12:34:03 -0500

    added avatars

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,12 +1,30 @@
 import { DeleteOutline } from "@mui/icons-material";
-import { Card, CardContent, CardHeader, IconButton, Typography } from "@mui/material";
+import { Avatar, Card, CardContent, CardHeader, IconButton, Typography } from "@mui/material";
+import { cyan, indigo, purple, red, teal } from "@mui/material/colors";
 
 export default function NoteCard({ note, handleDelete }) {
+
+  const colors = {
+    reminder : red[500],
+    todo : cyan[500],
+    work : indigo[500],
+    money : teal[500]
+  }
 
   return (
     <div>
       <Card elevation={1}>
         <CardHeader
+          avatar={
+            <Avatar sx={
+              note.category[0] == 'r' ? {bgcolor: colors.reminder} :
+              note.category[0] == 'w' ? {bgcolor: colors.work} :
+              note.category[0] == 't' ? {bgcolor: colors.todo} :
+              {bgcolor: colors.money}
+            }>
+              {note.category[0].toUpperCase()}
+            </Avatar>
+          }
           title={note.title}
           subheader={note.category}
           action={
